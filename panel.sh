f5ddee406ae11a49aa76e0c276103b13e7f3eb8c
Alexandr-Dolgov <aydolgov@gmail.com>
2016-06-13 22:36:26 +0500

    поиск панели через jps

---
+++ panel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#https://en.wikipedia.org/wiki/Shebang_(Unix)
+#http://unix.stackexchange.com/questions/206350/what-is-the-difference-if-i-start-bash-with-bin-bash-or-usr-bin-env-bash
+
+#это комментарий
+
+#---ЗАДАНИЕ---
+#Поиск панели через jps раз в 5 секунд
+#Если панель не найдена, дождаться пока она появится снова, получить json с /info/version и записать  в лог
+
+XXX=`jps -v | grep "\-DpanelName=" | cut -d " " -f1`
+if [ ${XXX} ]; then
+    echo "yes" #панель найдена через jsp
+else
+    echo "no" #панель не найдена через jsp
+fi
+
