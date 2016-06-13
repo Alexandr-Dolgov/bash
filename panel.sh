c9537905c8cc78629bdd62780f26125f6cd652dc
Alexandr-Dolgov <aydolgov@gmail.com>
2016-06-14 00:01:29 +0500

    Поиск панели через jps раз в 5 секунд. Если панель не найдена, дождаться пока она появится снова, получить json с /info/version и записать  в лог

--- panel.sh
+++ panel.sh
@@ -2,16 +2,33 @@
 #https://en.wikipedia.org/wiki/Shebang_(Unix)
 #http://unix.stackexchange.com/questions/206350/what-is-the-difference-if-i-start-bash-with-bin-bash-or-usr-bin-env-bash
 
-#это комментарий
-
 #---ЗАДАНИЕ---
 #Поиск панели через jps раз в 5 секунд
 #Если панель не найдена, дождаться пока она появится снова, получить json с /info/version и записать  в лог
 
-XXX=`jps -v | grep "\-DpanelName=" | cut -d " " -f1`
-if [ "$XXX" != "" ]; then
-    echo "yes" #панель найдена через jsp
-else
-    echo "no" #панель не найдена через jsp
-fi
+PANEL_URL="http://localhost:8080/first"
 
+writeToLog=false
+
+while true; do
+    XXX=`jps -v | grep "\-DpanelName=" | cut -d " " -f1`
+    if [ "$XXX" != "" ]; then
+        echo "yes" #панель найдена через jsp
+        if [ "$writeToLog" = true ]; then
+            #получить json с /info/version и записать  в лог
+            response=`curl --silent "$PANEL_URL/info/version" | grep "revision"`
+            while [ "$response" = "" ]; do
+                printf "."
+                sleep 1
+                response=`curl --silent "$PANEL_URL/info/version" | grep "revision"`
+            done
+            echo "$response" >> log
+            echo "info"
+            writeToLog=false
+        fi
+    else
+        echo "no" #панель не найдена через jsp
+        writeToLog=true
+    fi
+    sleep 5
+done
